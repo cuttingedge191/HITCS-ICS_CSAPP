bc0e919dc0603e21463dc8aff94c1d6718adfaf1
cuttingedge191 <scy1359165346@163.com>
2021-04-22 23:26:57 +0800

    Lab3 phase6.

--- HIT_Labs/Lab3/bomb321/sim_phase_4.c
+++ HIT_Labs/Lab3/bomb321/sim_phase_4.c
@@ -1,37 +1,25 @@
 //模拟phase_4的处理过程
-/*存在问题：func4函数递归调用未在正确时机结束（似乎多递归了若干次）
-导致input1输出有错误值，且在主函数中无法输出*/
 #include <stdio.h>
 
-int eax, ebx, edx, edi, esi;
-
-void func4(void)
+int func4(int edx, int edi, int esi)
 {
-    int tmp;
+    int eax, ebx;
     eax = edx - esi;
-    ebx = (eax >> 31) & 0x1 + eax;
+    ebx = eax;
     ebx >>= 1;
     ebx += esi;
     if (ebx > edi)
     {
         edx = ebx - 1;
-        tmp = ebx; //模仿push %ebx，但似乎不太一样
-        func4();
-        ebx = tmp; //模仿pop %ebx，但似乎不太一样
-        ebx += eax;
+        ebx += func4(edx, edi, esi);
     }
     else if (ebx < edi)
     {
         esi = ebx + 1;
-        tmp = ebx; //模仿push %ebx，但似乎不太一样
-        func4();
-        ebx = tmp; //模仿pop %ebx，但似乎不太一样
-        ebx += eax;
+        ebx += func4(edx, edi, esi);
     }
     eax = ebx;
-    if (eax == 35)
-        printf("input1 = %d\n", edi);
-    return;
+    return eax;
 }
 
 int main(void)
@@ -40,11 +28,11 @@ int main(void)
     int res;
     for (input1 = 0; input1 <= 14; ++input1)
     {
-        edx = 14;
-        esi = 0;
-        edi = input1;
-        //寄存器eax、ebx当前值未被利用，此处不进行赋值
-        func4();
+        res = func4(14, input1, 0);
+        if (res == 35)
+            printf("Password:[%d %d]\n", input1, 35);
+        else
+            printf("Wrong:[%d %d]\n", input1, 35);
     }
     return 0;
 }
