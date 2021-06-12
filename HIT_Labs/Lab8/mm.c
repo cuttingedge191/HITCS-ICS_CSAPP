8b0121be3d4ad7a34c744a0f87eedf2876d2ed1c
cuttingedge191 <scy1359165346@163.com>
2021-06-12 13:26:55 +0800

    Lab8 update.

--- HIT_Labs/Lab8/mm.c
+++ HIT_Labs/Lab8/mm.c
@@ -1,6 +1,7 @@
 /* 
- * mm-implicit.c -  Simple allocator based on implicit free lists, 
- *                  first fit placement, and boundary tag coalescing. 
+ * mm-implicit.c -> mm.c
+ * Simple allocator based on implicit free lists, 
+ * first fit placement, and boundary tag coalescing. 
  *
  * Each block has header and footer of the form:
  * 
@@ -37,20 +38,24 @@
 
 /* Team structure */
 team_t team = {
+    /* Team name */
 #ifdef NEXT_FIT
     "implicit next fit", 
 #else
-    "implicit first fit", 
+    "1190200526",
+    /* First member's full name */ 
 #endif
-    "Dave OHallaron", "droh",
+    "Chengyou Shen",
+    /* First member's email address */
+    "1190200526@stu.hit.edu.cn",
     "", ""
-}; 
+};
 
 /* $begin mallocmacros */
 /* Basic constants and macros */
 #define WSIZE       4       /* word size (bytes) */  
 #define DSIZE       8       /* doubleword size (bytes) */
-#define CHUNKSIZE  (1<<12)  /* initial heap size (bytes) */
+#define CHUNKSIZE  (1<<8)  /* initial heap size (bytes) 原：(1<<12)*/
 #define OVERHEAD    8       /* overhead of header and footer (bytes) */
 
 #define MAX(x, y) ((x) > (y)? (x) : (y))  
@@ -97,11 +102,11 @@ int mm_init(void)
 {
     /* create the initial empty heap */
     if ((heap_listp = mem_sbrk(4*WSIZE)) == NULL)
-	return -1;
-    PUT(heap_listp, 0);                        /* alignment padding */
-    PUT(heap_listp+WSIZE, PACK(OVERHEAD, 1));  /* prologue header */ 
-    PUT(heap_listp+DSIZE, PACK(OVERHEAD, 1));  /* prologue footer */ 
-    PUT(heap_listp+WSIZE+DSIZE, PACK(0, 1));   /* epilogue header */
+	    return -1;
+    PUT(heap_listp, 0);                        /* alignment padding （对齐填充）*/
+    PUT(heap_listp+WSIZE, PACK(OVERHEAD, 1));  /* prologue header （序言块头部）*/ 
+    PUT(heap_listp+DSIZE, PACK(OVERHEAD, 1));  /* prologue footer （序言块脚部）*/ 
+    PUT(heap_listp+WSIZE+DSIZE, PACK(0, 1));   /* epilogue header （结尾块，仅有头部）*/
     heap_listp += DSIZE;
 
 #ifdef NEXT_FIT
@@ -110,7 +115,7 @@ int mm_init(void)
 
     /* Extend the empty heap with a free block of CHUNKSIZE bytes */
     if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
-	return -1;
+	    return -1;
     return 0;
 }
 /* $end mminit */
@@ -127,24 +132,24 @@ void *mm_malloc(size_t size)
 
     /* Ignore spurious requests */
     if (size <= 0)
-	return NULL;
+	    return NULL;
 
     /* Adjust block size to include overhead and alignment reqs. */
     if (size <= DSIZE)
-	asize = DSIZE + OVERHEAD;
+	    asize = DSIZE + OVERHEAD;
     else
-	asize = DSIZE * ((size + (OVERHEAD) + (DSIZE-1)) / DSIZE);
+	    asize = DSIZE * ((size + (OVERHEAD) + (DSIZE-1)) / DSIZE); /* 向上取整 */
     
     /* Search the free list for a fit */
     if ((bp = find_fit(asize)) != NULL) {
-	place(bp, asize);
-	return bp;
+	    place(bp, asize);
+	    return bp;
     }
 
     /* No fit found. Get more memory and place the block */
     extendsize = MAX(asize,CHUNKSIZE);
     if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
-	return NULL;
+	    return NULL;
     place(bp, asize);
     return bp;
 } 
@@ -174,12 +179,12 @@ void *mm_realloc(void *ptr, size_t size)
     size_t copySize;
 
     if ((newp = mm_malloc(size)) == NULL) {
-	printf("ERROR: mm_malloc failed in mm_realloc\n");
-	exit(1);
+	    printf("ERROR: mm_malloc failed in mm_realloc\n");
+	    exit(1);
     }
     copySize = GET_SIZE(HDRP(ptr));
     if (size < copySize)
-      copySize = size;
+        copySize = size;
     memcpy(newp, ptr, copySize);
     mm_free(ptr);
     return newp;
@@ -193,22 +198,22 @@ void mm_checkheap(int verbose)
     char *bp = heap_listp;
 
     if (verbose)
-	printf("Heap (%p):\n", heap_listp);
+	    printf("Heap (%p):\n", heap_listp);
 
     if ((GET_SIZE(HDRP(heap_listp)) != DSIZE) || !GET_ALLOC(HDRP(heap_listp)))
-	printf("Bad prologue header\n");
+	    printf("Bad prologue header\n");
     checkblock(heap_listp);
 
     for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
-	if (verbose) 
-	    printblock(bp);
-	checkblock(bp);
+	    if (verbose) 
+	        printblock(bp);
+	    checkblock(bp);
     }
      
     if (verbose)
-	printblock(bp);
+	    printblock(bp);
     if ((GET_SIZE(HDRP(bp)) != 0) || !(GET_ALLOC(HDRP(bp))))
-	printf("Bad epilogue header\n");
+	    printf("Bad epilogue header\n");
 }
 
 /* The remaining routines are internal helper routines */
@@ -249,15 +254,15 @@ static void place(void *bp, size_t asize)
     size_t csize = GET_SIZE(HDRP(bp));   
 
     if ((csize - asize) >= (DSIZE + OVERHEAD)) { 
-	PUT(HDRP(bp), PACK(asize, 1));
-	PUT(FTRP(bp), PACK(asize, 1));
-	bp = NEXT_BLKP(bp);
-	PUT(HDRP(bp), PACK(csize-asize, 0));
-	PUT(FTRP(bp), PACK(csize-asize, 0));
+	    PUT(HDRP(bp), PACK(asize, 1));
+	    PUT(FTRP(bp), PACK(asize, 1));
+	    bp = NEXT_BLKP(bp);
+	    PUT(HDRP(bp), PACK(csize-asize, 0));
+	    PUT(FTRP(bp), PACK(csize-asize, 0));
     }
     else { 
-	PUT(HDRP(bp), PACK(csize, 1));
-	PUT(FTRP(bp), PACK(csize, 1));
+	    PUT(HDRP(bp), PACK(csize, 1));
+	    PUT(FTRP(bp), PACK(csize, 1));
     }
 }
 /* $end mmplace */
@@ -273,13 +278,13 @@ static void *find_fit(size_t asize)
 
     /* search from the rover to the end of list */
     for ( ; GET_SIZE(HDRP(rover)) > 0; rover = NEXT_BLKP(rover))
-	if (!GET_ALLOC(HDRP(rover)) && (asize <= GET_SIZE(HDRP(rover))))
-	    return rover;
+	    if (!GET_ALLOC(HDRP(rover)) && (asize <= GET_SIZE(HDRP(rover))))
+	        return rover;
 
     /* search from start of list to old rover */
     for (rover = heap_listp; rover < oldrover; rover = NEXT_BLKP(rover))
-	if (!GET_ALLOC(HDRP(rover)) && (asize <= GET_SIZE(HDRP(rover))))
-	    return rover;
+	    if (!GET_ALLOC(HDRP(rover)) && (asize <= GET_SIZE(HDRP(rover))))
+	        return rover;
 
     return NULL;  /* no fit found */
 #else 
@@ -287,9 +292,9 @@ static void *find_fit(size_t asize)
     void *bp;
 
     for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
-	if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
-	    return bp;
-	}
+	    if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) {
+	        return bp;
+	    }
     }
     return NULL; /* no fit */
 #endif
@@ -300,8 +305,36 @@ static void *find_fit(size_t asize)
  */
 static void *coalesce(void *bp) 
 {
-   /**/
-    return NULL;
+    size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HDRP(bp));
+
+    /* 情况1：前后均为已分配块 */
+    if (prev_alloc && next_alloc) {
+        return bp;
+    }
+    /* 情况2：前块已分配，后块空闲需合并 */
+    else if (prev_alloc && !next_alloc) {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+    /* 情况3：前块空闲需合并，后块已分配 */
+    else if (!prev_alloc && next_alloc) {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    /* 情况4：前后块均空闲，均要合并 */
+    else if (!prev_alloc && !next_alloc) {
+        size += GET_SIZE(HDRP(PREV_BLKP(bp))) + 
+            GET_SIZE(FTRP(NEXT_BLKP(bp)));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    return bp;
 }
 
 
@@ -331,4 +364,3 @@ static void checkblock(void *bp)
     if (GET(HDRP(bp)) != GET(FTRP(bp)))
 	printf("Error: header does not match footer\n");
 }
-
